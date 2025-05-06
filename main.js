fa2a0676467edf6d14f369c19bfeb051be046ee3
Miika Valkonen <miika.valkonen08@gmail.com>
2025-05-06 10:37:02 +0300

    Add images and enhance clicker game UI with upgrades and store features

--- main.js
+++ main.js
@@ -1,59 +1,53 @@
 document.getElementById("btnClick").addEventListener("click", click);
-document.getElementById("btnClickUpgrade").addEventListener("click", clickUpgrade)
-document.getElementById("btnPassiveUpgrade").addEventListener("click", passiveUpgrade)
-let clicksValue = 0
+document.getElementById("coffeeMachine").addEventListener("click", CoffeeMachine)
+document.getElementById("coffeeFactory").addEventListener("click", CoffeeFactory)
 
-// Clicks income
+// Money
+let Beans = 0
+
+// Money income
 let clickPower = 1
-let passivePower = 0
+let coffeeMachineIncome = 0
+let coffeeFactoryIncome = 0
 
-let clickUpgradeCost = 50
-let passiveUpgradeCost = 500
+// Money income upgrades
+let coffeeMachineCost = 10
+let coffeeFactoryCost = 100
+
+// Values
+let coffeeMachines = 0
+let coffeeFactorys = 0
 
 function click(event){
-    clicksValue += clickPower
+    Beans += clickPower
     updateClicks()
 }
 
 function updateClicks(){
     let clickText = document.getElementById("clicks");
     if (clickText) {
-        clickText.innerText = clicksValue;
+        clickText.innerText = Beans;
     } else {
         console.error("Element with id 'clicks' not found.");
     }
 }
 
-function clickUpgrade(){
-    if (clicksValue >= clickUpgradeCost) {
-        clicksValue = clicksValue - clickUpgradeCost
-        clickPower = clickPower * 2
-        clickUpgradeCost = clickUpgradeCost * 2
-        updateCosts()
-        updateClicks()
-    } else {
-        console.log("Rahat ei riitä click upgradeen")
-        return
-    }
-    
-}
-
 function passiveMoney(){
-    clicksValue += passivePower;
+    Beans += coffeeMachineIncome;
+    Beans += coffeeFactoryIncome;
     updateClicks();
 }
 
 setInterval(passiveMoney, 1000);
 
-function passiveUpgrade(){
-    if (clicksValue >= passiveUpgradeCost) {
-        clicksValue = clicksValue - passiveUpgradeCost
-        if (passivePower == 0){
-            passivePower = passivePower + 100
-        } else {
-            passivePower = passivePower * 2
-        }
-        passiveUpgradeCost = passiveUpgradeCost * 2
+// Upgrade Functions
+
+function CoffeeMachine(){
+    if (Beans >= coffeeMachineCost) {
+        Beans = Beans - coffeeMachineCost
+        coffeeMachineIncome += 1
+        coffeeMachineCost = coffeeMachineCost * 2
+        coffeeMachines += 1
         updateCosts()
         updateClicks()
     } else {
@@ -62,9 +56,29 @@ function passiveUpgrade(){
     }
 }
 
+function CoffeeFactory(){
+    if (Beans >= coffeeFactoryCost) {
+        Beans = Beans - coffeeFactoryCost
+        coffeeFactoryIncome += 10
+        coffeeFactoryCost = coffeeFactoryCost * 2
+        coffeeFactorys += 1
+        updateCosts()
+        updateClicks()
+    } else {
+        console.log("Rahat ei riitä click upgradeen")
+        return
+    }
+}
+
 function updateCosts(){
-    document.getElementById("clickUpgradeCost").innerText = clickUpgradeCost
-    document.getElementById("passiveUpgradeValue").innerText = "+" + passivePower
-    document.getElementById("passiveUpgradeCost").innerText = passiveUpgradeCost
-    document.getElementById("clickSecond").innerText = passivePower
+    // Hinnat
+    document.getElementById("coffeeMachineCostLabel").innerText = coffeeMachineCost + " Beans"
+    document.getElementById("coffeeFactoryCostLabel").innerText = coffeeFactoryCost + " Beans"
+
+    // Upgrade määrät
+    document.getElementById("coffeeMachineValue").innerText = coffeeMachines
+    document.getElementById("coffeeFactoryValue").innerText = coffeeFactorys
+
+    // Beans Per Second
+    document.getElementById("bps").innerText = "Beans per second: " + (coffeeFactoryIncome + coffeeMachineIncome)
 }
